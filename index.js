19b671459882d9a8eaa1bc917a557e483921a560
kuakuajiun <nancy003001@gmail.com>
2022-03-22 19:04:25 +0800

    Style modal & send request to show api + get data into modal

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function renderMovieList(data) {
 
   data.forEach((item) => {
     // title, image
-    console.log(item)
+    // console.log(item)
     rawHTML += `
       <div class="col-sm-3">
         <div class="mb-2">
@@ -24,7 +24,7 @@ function renderMovieList(data) {
             </div>
             <div class="card-footer">
               <button class="btn btn-primary btn-show-movie" data-toggle="modal"
-                data-target="#movie-modal">More</button>
+                data-target="#movie-modal" data-id="${item.id}">More</button>
               <button class="btn btn-info btn-add-favorite">+</button>
             </div>
           </div>
@@ -35,6 +35,28 @@ function renderMovieList(data) {
 
   dataPanel.innerHTML = rawHTML
 }
+
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+
+  axios.get(INDEX_URL + id).then(response => {
+    const data = response.data.results
+    modalTitle.innerHTML = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${POSTER_URL + data.image}" alt="movie-poster" class="img-fluid">`
+  })
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    // console.log(event.target.dataset)
+    showMovieModal(Number(event.target.dataset.id))
+  }
+})
 
 axios.get(INDEX_URL).then((response) => {
   // Array(80)
